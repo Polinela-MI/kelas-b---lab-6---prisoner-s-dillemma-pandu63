a61f1f511f5e132a9d15cc40c319c81f0574384b
pandu63 <pandurahmadi63@gmail.com>
2022-10-11 15:01:25 +0700

    Game

---
+++ Game.java
@@ -0,0 +1,22 @@
+public class Game {
+    private int Player1;
+    private int Player2;
+
+    public Game(int player1, int player2) {
+        Player1 = player1;
+        Player2 = player2;
+    }
+    public int getPlayer1() {
+        return Player1;
+    }
+    public void setPlayer1(int player1) {
+        Player1 = player1;
+    }
+    public int getPlayer2() {
+        return Player2;
+    }
+    public void setPlayer2(int player2) {
+        Player2 = player2;
+    }
+
+}
